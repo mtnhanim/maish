df99ae917867a88d6366678679396d3ff28630d5
mtnhanim <135309951+mtnhanim@users.noreply.github.com>
2023-06-12 23:11:53 +0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,12 @@
 const imageUrls = [
-    "img/gallery-img-01.jpg",
-    "img/gallery-img-02.jpg",
-    "img/gallery-img-03.jpg",
+    "img/mtn1.png",
+    "img/mtn3.jpg",
+    "img/mtn4.jpg",
+    "img/mtn5.jpg",
+    "img/mtn6.jpg",
+    "img/mtn8.jpg",
+    "img/mtn9.jpg",
+    "img/mtn10.jpg",
   ];
 
 const imageElement = document.getElementById("image-frame");
